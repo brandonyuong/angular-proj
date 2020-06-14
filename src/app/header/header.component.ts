3d6828d7f02b7654174f7818a0f0e0d12e39d05c
Brandon Yuong <brandonyuong@gmail.com>
2020-06-13 17:43:11 -0700

    Added navigation with event binding and ngIf.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
   collapsed: boolean = true;
 
   toggleCollapse(): void {
     this.collapsed = !this.collapsed;
+  }
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
 }
