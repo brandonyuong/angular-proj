1a13be6a6253e33215175696ed93bc6a746a8dff
Brandon Yuong <brandonyuong@gmail.com>
2020-06-16 22:04:54 -0700

    Implemented shopping list service.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,22 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 2)
+    ]),
+    new Recipe('Another test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    [
+      new Ingredient('Buns', 2),
+      new Ingredient('Meat', 3)
+    ])
   ];
 
   getRecipes() {
