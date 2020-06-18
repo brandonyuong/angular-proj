fbe3915caeda56ad16e469fcfc387ce9c9bef55f
Brandon Yuong <brandonyuong@gmail.com>
2020-06-17 22:15:25 -0700

    Added navigation to the app.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
   collapsed: boolean = true;
 
   toggleCollapse(): void {
     this.collapsed = !this.collapsed;
-  }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
   }
 }
