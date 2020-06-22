3f75cc87fc957cdfea4826786c7a1a593b2064ae
Brandon Yuong <brandonyuong@gmail.com>
2020-06-21 17:31:21 -0700

    Debugged subscription initiation in shopping-edit.component.ts.  startedEditing property was not being properly stored in this.subscription variable.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -20,7 +20,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
-    this.slService.startedEditing.subscribe(
+    this.subscription = this.slService.startedEditing.subscribe(
       (index: number) => {
         this.editedItemIndex = index;
         this.editMode = true;
