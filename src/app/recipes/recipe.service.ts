769750c3b2c959f457f6cb3ea5c3e39830db1920
Brandon Yuong <brandonyuong@gmail.com>
2020-06-22 22:08:32 -0700

    Implemented pipe for fetching recipes with no ingredients. (Transforming Response Data).

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,18 +9,20 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-    [
-      new Ingredient('Meat', 1),
-      new Ingredient('French Fries', 2)
-    ]),
-    new Recipe('Another test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-    [
-      new Ingredient('Buns', 2),
-      new Ingredient('Meat', 3)
-    ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+  //   [
+  //     new Ingredient('Meat', 1),
+  //     new Ingredient('French Fries', 2)
+  //   ]),
+  //   new Recipe('Another test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+  //   [
+  //     new Ingredient('Buns', 2),
+  //     new Ingredient('Meat', 3)
+  //   ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
