8e01efb8a636b4a695a3f7de3568a56df6b5d117
Brandon Yuong <brandonyuong@gmail.com>
2020-07-03 16:17:14 -0700

    Implemented Login Requests.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localID: string;
+  registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -34,5 +35,16 @@ export class AuthService {
       }
       return throwError(errorMessage);
     }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDDhtwHy_I2vxMP5x_g5Lswya7_nNmIVGc',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
   }
 }
